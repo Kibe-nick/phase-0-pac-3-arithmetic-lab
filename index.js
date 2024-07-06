12bc67dfa90f301101dd242368bb9051c132e59e
Nicholas Kibet <nickkorir08@gmail.com>
2024-07-06 16:11:00 +0300

    javaScript calc

---
+++ index.js
@@ -0,0 +1,34 @@
+function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    if (b === 0) {
+      throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+  }
+
+function add(a, b) {
+   return a + b; 
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
+  
